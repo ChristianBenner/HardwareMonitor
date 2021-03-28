b6a6969f41d815d0dfdf24146d3f57b16294c5b9
Chris Benner <christianbenner35@gmail.com>
2021-03-28 23:04:24 +0100

    Update Copyright Notice

--- src/com/bennero/server/Version.java
+++ src/com/bennero/server/Version.java
@@ -7,9 +7,11 @@
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * An additional term included with this license is the requirement to preserve legal notices and author attributions
- * such as this one. Do not remove the original author license notices from the program unless given permission from
- * the original author: christianbenner35@gmail.com
+ * Additional terms included with this license are to:
+ * - Preserve legal notices and author attributions such as this one. Do not remove the original author license notices
+ *   from the program
+ * - Preserve the donation button and its link to the original authors donation page (christianbenner35@gmail.com)
+ * - Only break the terms if given permission from the original author christianbenner35@gmail.com
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
